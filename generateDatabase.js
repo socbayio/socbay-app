c16d2bd68d410cabe4f562e7249bd272de693cd1
DESKTOP-ACLUKV6\trong <trongnghia.nguyen@outlook.com>
2021-05-06 00:38:39 +0200

    Add .gitignore

--- generateDatabase.js
+++ generateDatabase.js
@@ -1,144 +1,33 @@
 const mongoose = require('mongoose');
-const newVideo = require('./models/newVideosModel');
 const video = require('./models/videoModel');
 const user = require('./models/userModel')
-
-mongoose.connect("mongodb://localhost:27017/crustlive", {
+const liveChat = require('./models/liveChatModel');
+const global = require('./models/globalModel')
+const uploadBlock = require('./models/uploadBlockModel')
+mongoose.connect("mongodb://34.92.155.214:27028/socbay", {
     "auth": { "authSource": "admin" },
     "user": "ntn",
     "pass": "wv%nzw=VY$fMwV4",
     "useNewUrlParser": true
 });
 
-// a function in our model called create
-user.findOne({emailaddress: 'anonymous@ntn.com'}, (error, userfound)=>{
-    if (!userfound) {
-        user.create({
-            username: 'Anonymous Contributor',
-            emailaddress: 'anonymous@ntn.com',
-            password: 'zxcvbnm1@'
-        })
+
+liveChat.findOne({channel:'global'},(error,res)=>{
+    if (!res){
+        liveChat.create({channel:'global'});
     }
 })
-var anonymousID = "";
-var anonymousUsername = "";
-user.findOne({emailaddress: 'anonymous@ntn.com'}, (error, anonymous)=>{
-    anonymousID = anonymous._id;
-    anonymousUsername = anonymous.username;
-    //console.log(anonymousUsername,anonymousID)
 
-    video.create({
-        title: 'Trường Học Uy Long 1 (1991) – Phim Hài Châu Tinh Trì [Lồng Tiếng, HD]',
-        CID: 'QmNbmaa5gjw1WFSJ4rKNvJ9eCW8rWGgD7kkvsn6LqgAFn9',
-        thumbnail: '/images/courses/img-1.jpg',
-        author: {username: anonymousUsername, authorId: anonymousID}
-    },(error, videoCreated)=>{
-        newVideo.create({videoId: videoCreated._id});
-    });
-
-    video.create({
-        title: 'Amazing! Animal',
-        CID: 'QmdUkfAMwiyPnRGNoaGjuK1JEP4W8C4raoskkdTikUAsc4',
-        thumbnail: '/images/courses/img-1.jpg',
-        author: {username: anonymousUsername, authorId: anonymousID}
-    },(error, videoCreated)=>{
-        newVideo.create({videoId: videoCreated._id});
-    });
-
-    video.create({
-        title: 'Trường Học Uy Long 1 (1991) – Phim Hài Châu Tinh Trì [Lồng Tiếng, HD]',
-        CID: 'QmNbmaa5gjw1WFSJ4rKNvJ9eCW8rWGgD7kkvsn6LqgAFn9',
-        thumbnail: '/images/courses/img-1.jpg',
-        author: {username: anonymousUsername, authorId: anonymousID}
-    },(error, videoCreated)=>{
-        newVideo.create({videoId: videoCreated._id});
-    });
-
-    video.create({
-        title: 'Trường Học Uy Long 1 (1991) – Phim Hài Châu Tinh Trì [Lồng Tiếng, HD]',
-        CID: 'QmNbmaa5gjw1WFSJ4rKNvJ9eCW8rWGgD7kkvsn6LqgAFn9',
-        thumbnail: '/images/courses/img-1.jpg',
-        author: {username: anonymousUsername, authorId: anonymousID}
-    },(error, videoCreated)=>{
-        newVideo.create({videoId: videoCreated._id});
-    });
-
-    video.create({
-        title: 'Trường Học Uy Long 1 (1991) – Phim Hài Châu Tinh Trì [Lồng Tiếng, HD]',
-        CID: 'QmNbmaa5gjw1WFSJ4rKNvJ9eCW8rWGgD7kkvsn6LqgAFn9',
-        thumbnail: '/images/courses/img-1.jpg',
-        author: {username: anonymousUsername, authorId: anonymousID}
-    },(error, videoCreated)=>{
-        newVideo.create({videoId: videoCreated._id});
-    });
-
-    video.create({
-        title: 'Amazing! Animal',
-        CID: 'QmdUkfAMwiyPnRGNoaGjuK1JEP4W8C4raoskkdTikUAsc4',
-        thumbnail: '/images/courses/img-1.jpg',
-        author: {username: anonymousUsername, authorId: anonymousID}
-    },(error, videoCreated)=>{
-        newVideo.create({videoId: videoCreated._id});
-    });
-
-    video.create({
-        title: 'Trường Học Uy Long 1 (1991) – Phim Hài Châu Tinh Trì [Lồng Tiếng, HD]',
-        CID: 'QmNbmaa5gjw1WFSJ4rKNvJ9eCW8rWGgD7kkvsn6LqgAFn9',
-        thumbnail: '/images/courses/img-1.jpg',
-        author: {username: anonymousUsername, authorId: anonymousID}
-    },(error, videoCreated)=>{
-        newVideo.create({videoId: videoCreated._id});
-    });
-
-    video.create({
-        title: 'Trường Học Uy Long 1 (1991) – Phim Hài Châu Tinh Trì [Lồng Tiếng, HD]',
-        CID: 'QmNbmaa5gjw1WFSJ4rKNvJ9eCW8rWGgD7kkvsn6LqgAFn9',
-        thumbnail: '/images/courses/img-1.jpg',
-        author: {username: anonymousUsername, authorId: anonymousID}
-    },(error, videoCreated)=>{
-        newVideo.create({videoId: videoCreated._id});
-    });
-
-    video.create({
-        title: 'Trường Học Uy Long 1 (1991) – Phim Hài Châu Tinh Trì [Lồng Tiếng, HD]',
-        CID: 'QmNbmaa5gjw1WFSJ4rKNvJ9eCW8rWGgD7kkvsn6LqgAFn9',
-        thumbnail: '/images/courses/img-1.jpg',
-        author: {username: anonymousUsername, authorId: anonymousID}
-    },(error, videoCreated)=>{
-        newVideo.create({videoId: videoCreated._id});
-    });
-
-    video.create({
-        title: 'Amazing! Animal',
-        CID: 'QmdUkfAMwiyPnRGNoaGjuK1JEP4W8C4raoskkdTikUAsc4',
-        thumbnail: '/images/courses/img-1.jpg',
-        author: {username: anonymousUsername, authorId: anonymousID}
-    },(error, videoCreated)=>{
-        newVideo.create({videoId: videoCreated._id});
-    });
-
-    video.create({
-        title: 'Trường Học Uy Long 1 (1991) – Phim Hài Châu Tinh Trì [Lồng Tiếng, HD]',
-        CID: 'QmNbmaa5gjw1WFSJ4rKNvJ9eCW8rWGgD7kkvsn6LqgAFn9',
-        thumbnail: '/images/courses/img-1.jpg',
-        author: {username: anonymousUsername, authorId: anonymousID}
-    },(error, videoCreated)=>{
-        newVideo.create({videoId: videoCreated._id});
-    });
-
-    video.create({
-        title: 'Trường Học Uy Long 1 (1991) – Phim Hài Châu Tinh Trì [Lồng Tiếng, HD]',
-        CID: 'QmNbmaa5gjw1WFSJ4rKNvJ9eCW8rWGgD7kkvsn6LqgAFn9',
-        thumbnail: '/images/courses/img-1.jpg',
-        author: {username: anonymousUsername, authorId: anonymousID}
-    },(error, videoCreated)=>{
-        newVideo.create({videoId: videoCreated._id});
-    });
-
+global.findOne({variableName:'updateblock'},(error,res)=>{
+    if (!res){
+        global.create({variableName:'updateblock',currentBlock:0});
+    }
 })
 
-
-
-
+uploadBlock.findOne({blockNumber:3},(error,res)=>{
+    if (!res){
+        uploadBlock.create({totalSizeInByte:0,blockNumber:3,filesInfo:[]});
+    }
+})
 
 
