94960b813b157b16ad4d919d143485722416510e
DESKTOP-ACLUKV6\trong <trongnghia.nguyen@outlook.com>
2021-05-07 23:02:02 +0200

    - Chage time format of logger

---
+++ config.js
@@ -0,0 +1,12 @@
+const config = {
+    "userAuth":{
+        "auth": { "authSource": "admin" },
+        "user": "ntn",
+        "pass": "wv%nzw=VY$fMwV4",
+        "useNewUrlParser": true
+    },
+    "dbServerUrl": "mongodb://localhost:27028/",
+    secretKeyExpressSession: "keyboard cat"
+}
+
+module.exports = config;
