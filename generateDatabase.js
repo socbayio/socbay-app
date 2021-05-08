f873ad013dfa53d0615d941cea994f9fbd6e13ff
DESKTOP-ACLUKV6\trong <trongnghia.nguyen@outlook.com>
2021-05-08 23:55:35 +0200

    Rectify uploadblock foo

--- generateDatabase.js
+++ generateDatabase.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const video = require('./models/videoModel');
 const user = require('./models/userModel')
 const liveChat = require('./models/liveChatModel');
-const global = require('./models/globalModel')
 const uploadBlock = require('./models/uploadBlockModel')
+const globalConfig = require('./models/globalConfigModel')
 mongoose.connect("mongodb://34.92.155.214:27028/socbay", {
     "auth": { "authSource": "admin" },
     "user": "ntn",
@@ -11,23 +11,24 @@ mongoose.connect("mongodb://34.92.155.214:27028/socbay", {
     "useNewUrlParser": true
 });
 
+globalConfig.create({variableName:"updateblock", currentBlock:6});
 
-liveChat.findOne({channel:'global'},(error,res)=>{
-    if (!res){
-        liveChat.create({channel:'global'});
-    }
-})
+// liveChat.findOne({channel:'global'},(error,res)=>{
+//     if (!res){
+//         liveChat.create({channel:'global'});
+//     }
+// })
 
-global.findOne({variableName:'updateblock'},(error,res)=>{
-    if (!res){
-        global.create({variableName:'updateblock',currentBlock:0});
-    }
-})
+// global.findOne({variableName:'updateblock'},(error,res)=>{
+//     if (!res){
+//         global.create({variableName:'updateblock',currentBlock:0});
+//     }
+// })
 
-uploadBlock.findOne({blockNumber:3},(error,res)=>{
-    if (!res){
-        uploadBlock.create({totalSizeInByte:0,blockNumber:3,filesInfo:[]});
-    }
-})
+// uploadBlock.findOne({blockNumber:3},(error,res)=>{
+//     if (!res){
+//         uploadBlock.create({totalSizeInByte:0,blockNumber:3,filesInfo:[]});
+//     }
+// })
 
 
