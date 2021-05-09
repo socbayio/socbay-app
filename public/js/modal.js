254afbf116e6875436ad60fbaf6c92d70613ad7c
VU Tuan Trung <trung.lespace@gmail.com>
2021-05-09 19:32:24 +0200

    adding notification for web beta version

---
+++ public/js/modal.js
@@ -0,0 +1,47 @@
+// (function (window, document, undefined) {
+//     'use strict';
+
+//     var modal = document.getElementsByClassName('modalDemoWeb')[0];
+
+//     var modalBg = document.getElementsByClassName('modalDemoBg')[0];
+//     console.log(modalBg);
+
+//     modalBg.addEventListener('click', function () {
+//         console.log('bg clicked');
+
+//         localStorage.setItem('disableNotification', true);
+//         modal.style.display = 'none';
+//     });
+
+//     var disableBtn = document.getElementsByClassName('disable_model_btn')[0];
+//     disableBtn.addEventListener('click', function () {
+//         console.log('btn clicked');
+
+//         localStorage.setItem('disableNotification', true);
+//         modal.style.display = 'none';
+//     });
+// });
+
+console.log('reload page');
+
+//localStorage.setItem('disableNotification', false);
+
+var modal = document.getElementsByClassName('modalDemoWeb')[0];
+
+var modalBg = document.getElementsByClassName('modalDemoBg')[0];
+console.log(modalBg);
+
+modalBg.addEventListener('click', function () {
+    console.log('bg clicked');
+
+    localStorage.setItem('disableNotification', true);
+    modal.style.display = 'none';
+});
+
+var disableBtn = document.getElementsByClassName('disable_model_btn')[0];
+disableBtn.addEventListener('click', function () {
+    console.log('btn clicked');
+
+    localStorage.setItem('disableNotification', true);
+    modal.style.display = 'none';
+});
