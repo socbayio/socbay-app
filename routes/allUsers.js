f8a6dc145b6c11fc8097f69df4fba291712610d2
VU Tuan Trung <trung.lespace@gmail.com>
2021-05-11 15:52:51 +0200

    get and display all the users

--- routes/allUsers.js
+++ routes/allUsers.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const getInfoIfAuthenticated = require('../middleware/getInfoIfAuthenticated.js');
+const { getAllUsers } = require('./common');
 
-router.get('/', function (req, res, next) {
-    res.render('allUsers');
+router.get('/', getInfoIfAuthenticated, async (req, res, next) => {
+    try {
+        const users = await getAllUsers();
+        res.render('allUsers', { users });
+    } catch (error) {}
 });
 
 module.exports = router;
