4588c3d0d00c20521f9ae0a8cb34ec78070af168
VUTuanTrung <trung.lespace@gmail.com>
2021-05-13 15:54:29 +0200

    update puulvideo route, add checking if author subscribed

--- routes/pullvideo.js
+++ routes/pullvideo.js
@@ -4,28 +4,52 @@ const Video = require('../models/videoModel.js');
 const liveChatVideo = require('../models/liveChatVideoModel');
 const getInfoIfAuthenticated = require('../middleware/getInfoIfAuthenticated.js');
 const liveChat = require('../models/liveChatModel');
+const { isEmptyObject } = require('./common');
+
 const gateway = {
     ipfs: 'https://ipfs.io/ipfs/',
     crust: 'https://crustwebsites.net/ipfs/',
     oneloveipfs: 'https://video.oneloveipfs.com/ipfs/',
 };
 
-router.get('/', getInfoIfAuthenticated, async (req, res, next) => {
+function checkSubscribed(req, res, next) {
+    if (!isEmptyObject(req.userInfo)) {
+        req.userInfo.authorSubscribeb = false;
+        const authorSubscribed = req.userInfo.subscriptionsId.find(
+            (s) => s.userId === req.userInfo.videoAuthorId
+        );
+        req.userInfo.authorSubscribeb = authorSubscribed !== null;
+    }
+    next();
+}
+
+async function increaseView(req, res, next) {
+    const videoFound = await Video.findByIdAndUpdate(req.params.videoId, {
+        $inc: { view: 1 },
+    });
+    next();
+}
+
+async function getVideo(req, res, next) {
     let link = 'https://ipfs.io/ipfs/';
     if (req.query.gateway in gateway) {
         link = gateway[req.query.gateway];
     }
 
     try {
-        const videoFound = await Video.findByIdAndUpdate(req.params.videoId, {
-            $inc: { view: 1 },
-        }).populate('authorId', 'profilePicture username');
+        const videoFound = await Video.findById(req.params.videoId).populate(
+            'authorId',
+            'profilePicture username'
+        );
+
         //const liveChatVideoFound = await liveChatVideo.findOne({videoId: req.params.videoId});
         const liveChatVideoFound = await liveChat.findOne({
             channel: 'global',
-        }); // Just for test
+        });
+
+        // Just for test
         if (videoFound && liveChatVideoFound) {
-            let videoInfo = {
+            req.videoInfo = {
                 videoId: req.params.videoId,
                 link: link + videoFound.networkStatus.CID,
                 title: videoFound.title,
@@ -36,14 +60,11 @@ router.get('/', getInfoIfAuthenticated, async (req, res, next) => {
                 videoAuthorPicture: videoFound.authorId.profilePicture,
                 description: videoFound.description,
             };
-            let liveChat = {
+            req.liveChat = {
                 messages: liveChatVideoFound.messages,
             };
-            res.render('video', {
-                userInfo: req.userInfo,
-                liveChat,
-                videoInfo,
-            });
+
+            next();
         } else {
             throw new Error('No livechat | No video');
         }
@@ -51,6 +72,23 @@ router.get('/', getInfoIfAuthenticated, async (req, res, next) => {
         console.error(`Pull video fail with error: ${e}`);
         next(e);
     }
-});
+}
+
+function render(req, res, next) {
+    res.render('video', {
+        userInfo: req.userInfo,
+        liveChat: req.liveChat,
+        videoInfo: req.videoInfo,
+    });
+}
+
+router.get(
+    '/',
+    getInfoIfAuthenticated,
+    increaseView,
+    getVideo,
+    checkSubscribed,
+    render
+);
 
 module.exports = router;
