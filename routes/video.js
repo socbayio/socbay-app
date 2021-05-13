63633fc5b22cae73f2ee2b0aed2b9394973e0984
VUTuanTrung <trung.lespace@gmail.com>
2021-05-13 13:20:39 +0200

    add userid to array subscriptions

--- routes/video.js
+++ routes/video.js
@@ -7,7 +7,22 @@ const User = require('../models/userModel.js');
 async function getAuthorInfo(req, res, next) {
     const authorInfo = await User.findById(req.body.authorId);
     req.authorInfo = authorInfo;
-    res.send({ userInfo: req.userInfo, authorInfo: authorInfo });
+    // res.send({ userInfo: req.userInfo, authorInfo: authorInfo });
+    next();
+}
+
+async function subscribeUser(req, res, next) {
+    const userUpdated = await User.findOneAndUpdate(
+        { _id: req.userInfo.userId },
+        {
+            $push: {
+                subscriptions: req.authorInfo._id,
+            },
+        }
+    );
+    const userInfo = await User.findById(req.userInfo.userId);
+
+    res.send({ updateduserInfo: userInfo, authorInfo: req.authorInfo });
 }
 
 router.get('/', getInfoIfAuthenticated, function (req, res, next) {
@@ -18,7 +33,8 @@ router.post(
     '/author/subscribe',
     redirectIfNotAuthenticatedMiddleware,
     getInfoIfAuthenticated,
-    getAuthorInfo
+    getAuthorInfo,
+    subscribeUser
 );
 
 module.exports = router;
