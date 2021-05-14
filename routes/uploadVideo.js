81ff7809e0073a6ca4c42c430dafba98d50a248b
VU Tuan Trung <trung.lespace@gmail.com>
2021-05-14 17:52:49 +0200

    upload image and video to server

--- routes/uploadVideo.js
+++ routes/uploadVideo.js
@@ -4,6 +4,14 @@ const redirectIfNotAuthenticatedMiddleware = require('../middleware/redirectIfNo
 var router = express.Router();
 const User = require('../models/userModel.js');
 
+const { addFileInfo, addFileToIPFSPromise } = require('./common');
+const {
+    uploadBlockToCrust,
+    createNewBlock,
+} = require('../crust-socbay-pinner');
+const path = require('path');
+const fs = require('fs');
+
 router.get(
     '/',
     redirectIfNotAuthenticatedMiddleware,
@@ -13,15 +21,95 @@ router.get(
     }
 );
 
-function getDataFromReq(req, res, next) {
-    const body = req.body;
+async function saveThums(req, res, next) {
+    try {
+        const thumbnail = req.files.thumbnail;
+        // pass to next middleware if no thumbnail
+        if (!thumbnail) next();
+
+        let savingPath = path.resolve(
+            __dirname,
+            '..',
+            'public/images/thumbnails',
+            thumbnail.name
+        );
+        await thumbnail.mv(savingPath);
+    } catch (error) {
+        req.uploadResult.error.push(error.message);
+    } finally {
+        next();
+    }
+}
+
+async function saveVideo(req, res, next) {
+    try {
+        const video = req.files.video;
+        if (!video) next();
+
+        let localCurrentBlock = {
+            totalSize: globalCurrentBlock.totalSize,
+            blockNumber: globalCurrentBlock.blockNumber,
+        };
+        globalCurrentBlock.totalSize += video.size;
+        if (globalCurrentBlock.totalSize > 100 * 1024 * 1024) {
+            globalCurrentBlock.blockNumber++;
+            globalCurrentBlock.totalSize = 0;
+            createNewBlock();
+        }
+
+        let savingPath = path.resolve(
+            __dirname,
+            '..',
+            'public/videos',
+            localCurrentBlock.blockNumber.toString(),
+            video.name
+        );
+        await video.mv(savingPath);
+
+        const CIDOutput = await addFileToIPFSPromise(savingPath);
+        addFileInfo(
+            localCurrentBlock.blockNumber,
+            video.name,
+            video.size,
+            output
+        );
+        if (localCurrentBlock.totalSize + video.size > 100 * 1024 * 1024) {
+            uploadBlockToCrust(
+                config.crustPrivateKey,
+                localCurrentBlock.blockNumber
+            );
+        }
+
+        req.uploadResult.CID = CIDOutput;
+    } catch (error) {
+        req.uploadResult.error.push(error.message);
+    } finally {
+        next();
+    }
+}
+
+function prepareResult(req, res, next) {
+    req.uploadResult = {
+        state: '',
+        CID: '',
+        error: [],
+    };
+    next();
+}
+
+function returnResponse(req, res, next) {
+    if (req.uploadResult.error.length === 0) req.uploadResult.state = 'ok';
+    res.send({ res: req.uploadResult });
 }
 
 router.post(
     '/testUploadFiles',
     redirectIfNotAuthenticatedMiddleware,
     getInfoIfAuthenticated,
-    getDataFromReq
+    prepareResult,
+    saveThums,
+    saveVideo,
+    returnResponse
 );
 
 module.exports = router;
