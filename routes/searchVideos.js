d01ce948362db686c52cc1381f844f17974d7e2b
DESKTOP-ACLUKV6\trong <trongnghia.nguyen@outlook.com>
2021-05-15 20:32:40 +0200

    Fix bug search page

--- routes/searchVideos.js
+++ routes/searchVideos.js
@@ -1,28 +1,32 @@
 var express = require('express');
 var router = express.Router();
 const Video = require('../models/videoModel.js');
+const User = require('../models/userModel');
 
 router.get('/', (req, res) => {
-    var emailaddress = '';
-    var username = '';
-
-    Video.find({ $text: { $search: req.query.keyword } }, (error, result) => {
-        if (req.session.userId) {
-            User.findById(req.session.userId, (error, user) => {
-                username = user.username;
-                emailaddress = user.emailaddress;
+    try {
+        var emailaddress = '';
+        var username = '';
+        Video.find({ $text: { $search: req.query.keyword } }, (error, result) => {
+            if (req.session.userId) {
+                User.findById(req.session.userId, (error, user) => {
+                    username = user.username;
+                    emailaddress = user.emailaddress;
+                    return res.render('index', {
+                        emailaddress: emailaddress,
+                        username: username,
+                        renderVideos: [{ name: 'searchresults', videos: result }],
+                    });
+                });
+            } else {
                 return res.render('index', {
-                    emailaddress: emailaddress,
-                    username: username,
                     renderVideos: [{ name: 'searchresults', videos: result }],
                 });
-            });
-        } else {
-            return res.render('index', {
-                renderVideos: [{ name: 'searchresults', videos: result }],
-            });
-        }
-    });
+            }
+        });
+    } catch (e) {
+        console.error(`Error search page ${e}`);
+    }
 });
 
 module.exports = router;
