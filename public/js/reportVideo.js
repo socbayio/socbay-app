4650eccae2239e69005560e333be59c1fe9c8a0f
VUTuanTrung <trung.lespace@gmail.com>
2021-05-17 14:15:39 +0200

    add some css style and move code to js file

--- public/js/reportVideo.js
+++ public/js/reportVideo.js
@@ -1,1 +1,11 @@
+$(document).ready(function () {
+    const reportSpan = document.getElementById('reportSpan');
+    const popup = document.getElementsByClassName('reportVideo')[0];
+
+    reportSpan.addEventListener('click', function (e) {
+        console.log('report span clicked');
+        popup.style.display = 'flex';
+    });
+});
+
 function reportVideo(videoId, reportId, descriptions) {}
