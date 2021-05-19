6778f8afb45ecd33ffd98369fa99627f21115075
DESKTOP-ACLUKV6\trong <trongnghia.nguyen@outlook.com>
2021-05-20 00:13:31 +0200

    Fix search page

--- routes/searchVideos.js
+++ routes/searchVideos.js
@@ -3,16 +3,33 @@ var router = express.Router();
 const Video = require('../models/videoModel.js');
 const User = require('../models/userModel');
 const getInfoIfAuthenticated = require('../middleware/getInfoIfAuthenticated.js');
+const logger = require('../logger').Logger;
 
 router.get('/', getInfoIfAuthenticated, async (req, res) => {
     try {
-        const result = await Video.find({ $text: { $search: req.query.keyword } });
+        const result = await Video.find({$text: {$search: req.query.keyword}})
+            .populate('thumbnail.blockId', 'uploadedToNetwork CID');
+
+        await Promise.all(result.map(async (video) =>{
+            await video.thumbnail.subPopulate('fileId');
+            if (video.thumbnail.blockId.uploadedToNetwork) {
+                video.thumbnail = {
+                    link: video.thumbnail.blockId.CID + '/' + video.thumbnail.fileId.fileName
+                }
+            } else {
+                video.thumbnail = {
+                    link: video.thumbnail.fileId.CID
+                }
+            }
+            return video;
+        }));
+
         res.render('index', {
             userInfo: req.userInfo,
             renderVideos: [{ name: 'searchresults', videos: result }],
         });
     } catch (e) {
-        console.error(`Error search page ${e}`);
+        logger.error(`Error search page ${e}`);
     }
 });
 
