0f6233bfa249ad7e472b71cdd47c273620d256a8
DESKTOP-ACLUKV6\trong <trongnghia.nguyen@outlook.com>
2021-05-26 20:11:58 +0200

    Ignore secret info

--- ipfsconfig.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    seedsPath: './seeds',
-    chainAddr: 'wss://api.crust.network',
-    ipfsTimeout: 60000, // 60s
-    crustPrivateKey: "trial portion crucial caught word amused slot struggle need scissors polar curtainxx",
-}
