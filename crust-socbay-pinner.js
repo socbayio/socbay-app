d93be0939fdd92e21cf4bb0ddabf6f887be15013
Trong Nghia <trongnghia.nguyen@outlook.com>
2021-06-08 15:37:39 +0200

    Remove draft function

--- crust-socbay-pinner.js
+++ crust-socbay-pinner.js
@@ -4,13 +4,7 @@ var config = require('./config');
 var path = require('path');
 var logger = require('./logger').Logger;
 
-const { addFileInfo,
-    pushFileToMe,
-    pushVideoToMe,
-    pushVideoToTag,
-    
-} = require('./routes/common');
-
+const { addFileInfo } = require('./routes/common');
 
 const pin = require('./crust-ipfs/pin.js').default;
 const publish = require('./crust-ipfs/publish.js').default;
@@ -27,19 +21,6 @@ const createNewBlock = async ()=>{
     await uploadBlock.create({
         blockNumber: globalUploadBlockInfo._doc.currentBlock+1
     });
-}
-
-const uploadBlockToCrust = async(seed,blockNumber)=>{
-    try {
-        let pathFolderToUpload = path.resolve(__dirname,'public/block',blockNumber.toString());
-        logger.crustSocbayPinner(`----- START PINNING BLOCK ${blockNumber} -----`);
-        const pinnedFolder = await pin(pathFolderToUpload);
-        await uploadBlock.findOneAndUpdate({blockNumber},{CID: pinnedFolder.cid, timeStamp:Date.now()});
-        await publish(seed, pinnedFolder.cid, 0.00);
-        logger.crustSocbayPinner(`----- FINISH SUCCESSFULLY PINNING BLOCK ${blockNumber} -----`);
-    } catch (e) {
-        logger.crustSocbayPinner(`----- FINISH PINNING BLOCK ${blockNumber} WITH ERROR ${e} -----`);
-    }
 }
 
 /**
@@ -121,7 +102,6 @@ const uploadBlockToCrust = async(seed,blockNumber)=>{
 
 module.exports = { 
   createNewBlock,
-  uploadBlockToCrust,
   chooseStorageBlock,
   uploadFile
 };
